89a9163a08650f43507688a9b54b3161a9097271
gireeswar18 <gireeswarcp18@gmail.com>
2026-02-06 18:17:01 +0530

    trigger added

--- src/main/java/giree/sample/test/Controller.java
+++ src/main/java/giree/sample/test/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 
     @GetMapping("/")
     public String root() {
-        return "Spring Boot + Jenkins working fine!";
+        return "Spring Boot + Jenkins working fine! Trigger Added!";
     }
 
 }
